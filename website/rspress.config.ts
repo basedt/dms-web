ae92347124e820373e898085124058ccc150e5bb
gengleiyu <gengleiyu@xinxuan.net>
2025-02-25 18:21:38 +0800

    [DMS] support data import and export

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -1,61 +1,61 @@
-import * as path from 'node:path';
-import {defineConfig} from 'rspress/config';
+import * as path from "node:path";
+import { defineConfig } from "rspress/config";
 
 export default defineConfig({
-    root: path.join(__dirname, 'docs'),
-    // base: '/dms-web/',
-    title: 'Base DMS',
-    icon: '/logo.svg',
-    logo: {
-        light: '/logo.svg',
-        dark: '/logo.svg',
+  root: path.join(__dirname, "docs"),
+  // base: '/dms-web/',
+  title: "Base DMS",
+  icon: "/logo.svg",
+  logo: {
+    light: "/logo.svg",
+    dark: "/logo.svg",
+  },
+  logoText: "DMS",
+  lang: "zh",
+  locales: [
+    {
+      lang: "en",
+      label: "English",
+      title: "Base DMS",
+      description: "Base Data Managerment System",
     },
-    logoText: "DMS",
-    lang: 'zh',
-    locales: [
-        {
-            lang: 'en',
-            label: 'English',
-            title: 'Base DMS',
-            description: 'Base Data Managerment System',
-        },
-        {
-            lang: 'zh',
-            label: '简体中文',
-            title: 'Base DMS',
-            description: 'Base数据管理系统',
-        }
+    {
+      lang: "zh",
+      label: "简体中文",
+      title: "Base DMS",
+      description: "Base数据管理系统",
+    },
+  ],
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/basedt/dms-web",
+      },
     ],
-    themeConfig: {
-        socialLinks: [
-            {
-                icon: 'github',
-                mode: 'link',
-                content: 'https://github.com/basedt/dms-web',
-            },
-        ],
-        locales: [
-            {
-                lang: 'en',
-                label: 'English',
-                outlineTitle: 'ON THIS PAGE',
-                prevPageText: 'Previous Page',
-                nextPageText: 'Next Page',
-            },
-            {
-                lang: 'zh',
-                label: '简体中文',
-                outlineTitle: '目录',
-                prevPageText: '上一页',
-                nextPageText: '下一页',
-            }
-        ]
-    },
-    multiVersion: {
-        default: "v1.0.0",
-        versions: ['v1.0.0','dev']
-    },
-    search: {
-        versioned: true,
-    }
+    locales: [
+      {
+        lang: "en",
+        label: "English",
+        outlineTitle: "ON THIS PAGE",
+        prevPageText: "Previous Page",
+        nextPageText: "Next Page",
+      },
+      {
+        lang: "zh",
+        label: "简体中文",
+        outlineTitle: "目录",
+        prevPageText: "上一页",
+        nextPageText: "下一页",
+      },
+    ],
+  },
+  multiVersion: {
+    default: "dev",
+    versions: ["v1.0.0", "dev"],
+  },
+  search: {
+    versioned: true,
+  },
 });
